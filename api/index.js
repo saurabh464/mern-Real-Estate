dbe89533dac7ed03608a4276de1c69b20260a4b6
Saurabh kumar <saurabhkumar46485@gmail.com>
2024-06-12 18:47:26 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -3,9 +3,6 @@ import express from 'express';
 const app = express();
 
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
 
 app.listen(3000, () => {
   console.log('Server is running at 3000!');
